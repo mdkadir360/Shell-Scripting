3d4fec658881f85764f3abb99e32dd4e2d31f717
Ubuntu <adminuser@vm.b11orjlltveunpkiqedgauicea.cx.internal.cloudapp.net>
2024-05-23 18:28:00 +0000

    comment code added

---
+++ myscript/02_comments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "checking Comments"
+#This is a single line comment
+
+<<comment
+this
+is
+Munti
+Line Comments
+comment
---
+++ myscript/03_vardemo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+#Script to show how to use variables
+
+
+a=10
+name="Kadir"
+age=21
+
+
+
+
+echo "My name is $name and age is $age"
